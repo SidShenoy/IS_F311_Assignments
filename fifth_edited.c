0e686db6f6919a7b78e060e4970640f6b2e5a8c4
Siddhant Shenoy <siddhant8897@gmail.com>
2017-09-16 20:30:44 +0530

    Added 2 new parameters to 'genNextTreeString' func. Added 1 new parameter to 'Display2' function which helps discriminate between objects being drawn. Like, tree will have fruits but grass will not. I set the object value of tree to 1 and grass to 2. Also added a random function that creates 32 grass having gen b/w 0 to 3 and 3 trees having gen b/w 0 to 3.

---
+++ fifth_edited.c
@@ -0,0 +1,262 @@
+/*This program draws a tree
+using the following graftal :
+	1. 0->1[0]1[0]0
+	2. 1->11
+	3. [->[
+	4. ]->]
+the axiom for this graftal is 0 
+compile using : gcc fifth_edited.c -lglut -lGLU -lGL -lm
+*/
+
+// This program uses the formula,
+// T(n+1) = 1^(2^n).[T(n)].1^(2^n).[T(n)].T(n)
+
+#include<GL/glut.h>
+#include<math.h>
+#include<string.h>
+#include<stdio.h>
+#define PI 3.1416
+
+int pos;
+int genNextTreeString(char* treeString, char* ruleForOne, char* ruleForZero){
+
+	int prevLen = strlen(treeString);
+	if(prevLen > 1000000){
+		printf("Memory!!");
+		return -1;
+	}
+	char buffer[prevLen*10];
+	buffer[0]=0;
+	int iter = 0;
+	int index = 0;
+	while(iter < prevLen){
+		if(treeString[iter] == '1'){
+			strncat(buffer,ruleForOne,2);
+		}
+		else if(treeString[iter] == '0'){
+			strncat(buffer,ruleForZero,9);
+		}
+		else{
+			strncat(buffer,treeString+iter,1);
+		}
+		iter++;
+	}
+	strcpy(treeString,buffer);
+	//printf("%s",treeString);
+	return 0;
+}
+void draw(int gen, char *arr)
+{
+	int i;
+
+	int ones = pow(2,gen-1);	//Computes the number of 1s to be added = 1^(2^n)
+
+	int len = strlen(arr);		//Computes the length of T(n-1)
+
+	int count = ones * 2 + 5 + 3 * len; //one extra for terminating null character
+
+	char arr2[len];
+
+	if(len==0)
+	{
+		arr[0] = '0';		//arr is now equal to axiom
+		arr[1] = '\0';		//null termination
+	}
+
+	if(gen==0)
+	return;	
+
+	else
+	{
+		for(i=0;i<len;i++)
+		{
+			arr2[i] = arr[i];				//before T(n-1) is modified into T(n), we make a copy of T(n-1)
+		}
+
+		for(i=0;i<count-1;i++)
+		{	
+			if((i>=0 && i<ones) || (i>=ones+len+2 && i<ones*2+len+2))	//Add 1s according to formula
+			arr[i] = '1';
+			else if((i == ones) || (i == ones*2+len+2))			//Add [s according to formula
+			arr[i] = '[';
+			else if((i == ones + len + 1)||(i == ones*2+len*2+3))		//Add ]s according to formula
+			arr[i] = ']';
+			else if((i >= ones + 1 && i <= ones + len))			//Add T(n-1), first occurrence
+			{
+				arr[i] = arr2[i-ones-1];
+			}
+			else if((i >= ones*2+len+3) && (i<=ones*2+len*2+2))		//Add T(n-1), second occurrence
+			{
+				arr[i] = arr2[i-ones*2-len-3];
+			}
+			else								//Add T(n-1), third occurrence
+			{
+				arr[i] = arr2[i-ones*2-len*2-4];
+			}
+		}
+
+		arr[count-1] = '\0';							//null termination of T(n)
+
+	}
+}
+
+// Recursive function that actually draws the tree from output of 'draw' function 
+// level = depth of the current branch
+// angle = angle of the current branch
+// currX = x-coordinate of the tip of the current branch
+// currY = y-coordinate of the tip of the current branch
+// arr = output string of 'draw' function 
+
+void Display2(int level,float angle,float currX,float currY,char *arr,int object)		
+{
+	int count = 1;					//needed to alternate between left and right branch drawings
+
+	//int size = 10/level;				//length of the stem to be drawn in the current branch
+	int size = 10;
+	for(;pos<strlen(arr);pos++)
+	{
+		if(arr[pos] == '1')			// '1' indicates that a stem in the current branch is to be drawn
+		{
+			glBegin(GL_LINES);				
+				glVertex2f(currX,currY);
+				glVertex2f(currX + size*cos((angle)*PI/180),currY + size*sin((angle)*PI/180));
+				currX = currX + size*cos((angle)*PI/180);
+				currY = currY + size*sin((angle)*PI/180);
+			glEnd();
+		}
+		else if(arr[pos]=='[')			// '[' indicates that a new branch is about to start
+		{
+			if(count%2==1)			// for new left branches
+			{
+				if(angle!=180)		 
+				{
+					pos++;					
+					Display2(level+1,angle+45,currX,currY,arr,object);
+					count++;
+				}
+				else			// if current angle is 180 degrees, new branch must be right branch
+				{
+					pos++;					
+					Display2(level+1,angle-45,currX,currY,arr,object);
+					count++;
+				}
+			}
+			else				// for new right branches
+			{
+				if(angle!=0)		
+				{
+					pos++;					
+					Display2(level+1,angle-45,currX,currY,arr,object);
+					count++;
+				}
+				else			// if current angle is 0 degrees, new branch must be left branch
+				{
+					pos++;					
+					Display2(level+1,angle+45,currX,currY,arr,object);
+					count++;
+				}
+			}			
+		}
+		else if(arr[pos]==']')			// indicates the end of the current branch
+		{	
+			if(object==1)	
+			{		
+				glPointSize(5.0);		// sets square fruit size to be = 5 pixels
+			
+				glColor3f(1.0,0.0,0.0);		// with RED color
+
+				glBegin(GL_POINTS);			
+					glVertex2f(currX,currY);	// draws the fruit				
+				glEnd();
+
+				glPointSize(1.0);		// switches point size back to 1 pixel
+				glColor3f(0.0,1.0,0.0);		// switches color back to green	
+			}
+
+			return;
+		}
+	}
+}
+
+void Display(void)
+{
+	int i,j;
+	float x = 0.0, y = 50.0;	
+
+	glClear(GL_COLOR_BUFFER_BIT);			
+
+	glColor3f(0.0,1.0,0.0);
+
+	char arr[10000];		// array to store string representing the tree
+	
+	arr[0] = '\0';			// null termination
+
+	for(i=0;i<3;i++)
+	{
+		char mainString[1000000]="0";
+
+		int gen1 = random()%3 + 1; // setting the generation of the object to be drawn
+
+		for(j=0;j<=gen1;j++)
+			genNextTreeString(mainString,"11","1[0]1[0]0");
+
+		x+=125;		
+
+		pos = 0;			// pos attribute to store how much of the tree has been traversed till now
+
+		Display2(1,90.0,x,50.0,mainString,1); // Initial call to recursive function 'Display2' to draw trees
+	}	
+
+	x = 0;
+
+	for(i=0;i<32;i++)
+	{
+		char mainString2[1000000]="1[0]";
+
+		int gen2 = random()%4;
+					
+		for(j=0;j<=gen2;j++)
+			genNextTreeString(mainString2,"1","1]1[0");
+
+		x+=15;		
+
+		pos = 0;
+		
+		Display2(1,90.0,x,50.0,mainString2,2);
+	}	
+		
+	glFlush();			// forcibly write contents of the buffer to the viewport
+}
+
+void reshape(int w,int h)		// when window is reshaped, draw the tree again by resizing viewport accordingly 
+{
+	glViewport(0,0,(GLsizei)w,(GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0,500,0.0,500);
+}
+
+void init(void)
+{
+	glClearColor(0.0,0.0,0.5,1.0);	//background color		
+}
+
+void main(int argc, char* argv[])
+{	
+	glutInit(&argc, argv);	
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("My first opengl window");
+	init();
+	glutDisplayFunc(Display);	
+	glutReshapeFunc(reshape);
+	glutMainLoop();			//keep looping until the user exits
+}
+
+
+
+
+
+
+
+
